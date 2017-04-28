db78b5eaaee3ff53d2bf1e16e78728168f6b76e9
andrasat <andra.satria1@gmail.com>
2017-04-28 18:20:07 +0700

    added action test

--- src/Action.test.js
+++ src/Action.test.js
@@ -2,8 +2,7 @@ import {
   fetchDataSuccess,
   addData,
   editData,
-  deleteData,
-  fetchData
+  deleteData
 } from './actions'
 
 import {
@@ -15,12 +14,38 @@ import {
 
 describe('Action test', () => {
 
-  it('Should add data to state', () => {
-    const payload = { username: 'testing', password: 'testing', url: 'http://www.url.com', createdAt: new Date() }
+  it('Should add 1 data', () => {
+    const payload = { id: 4, username: 'testing', password: 'testing', url: 'http://www.url.com', createdAt: new Date() }
     const expectedAction = {
       type: ADD_DATA,
       payload
     }
     expect(addData(payload)).toEqual(expectedAction)
+  })
+
+  it('Should delete 1 data with ID = 1', () => {
+    const payload = 1
+    const expectedAction = {
+      type: DELETE_DATA,
+      payload
+    }
+    expect(deleteData(payload)).toEqual(expectedAction)
+  })
+
+  it('Should success fetch data', () => {
+    const expectedAction = {
+      type: FETCH_DATA_SUCCESS,
+      payload: []
+    }
+    expect(fetchDataSuccess([])).toEqual(expectedAction)
+  })
+
+  it('Should edit username data with ID 1', ()=> {
+    const payload = { id: 1, username: 'changed'}
+    const expectedAction = {
+      type: EDIT_DATA,
+      payload
+    }
+    expect(editData(payload)).toEqual(expectedAction)
   })
 })
